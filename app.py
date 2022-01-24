5d33444bc3dc8714bdd649e2258e551d627d2028
Pavel <KuroKimala@gmail.com>
2022-01-24 14:21:14 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db =  SQLAlchemy(app)
 
 
@@ -29,9 +30,22 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/create-article')
+@app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
-    return render_template("create-article.html")
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+        return render_template("create-article.html")
 
 
 #@app.route('/user/<string:name>/<int:id>')
